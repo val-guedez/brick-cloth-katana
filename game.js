f81dc12845d38d525a6fa643676d2726577253ce
val-guedez <valentina.guedez.mercado@gmail.com>
2025-04-12 14:41:59 +1000

    Wrote basic getComputerChoice function in game.js

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
-console.log("hello there...");
+const options = ["brick", "cloth", "katana"];
+
+function getComputerChoice() {
+    const index = (Math.random() * 10) % 3;
+
+    switch (index) {
+        case 0:
+            return "brick";
+        case 1:
+            return "cloth";
+        case 2:
+            return "katana";
+        default:
+            throw new Error("erm what the sigma...");
+    }
+}
