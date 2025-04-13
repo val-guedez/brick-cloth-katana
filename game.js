60b3b71178e104fcbdfe0fe864053927f8a6c1a3
val-guedez <valentina.guedez.mercado@gmail.com>
2025-04-13 19:12:46 +1000

    Finish transitioning to ui (without styling)

--- game.js
+++ game.js
@@ -6,21 +6,51 @@ const KATANA = 2;
 const ROUND_LENGTH = 5;
 let humanScore = 0;
 let computerScore = 0;
+let roundCount = 0;
 
-gameLoop();
+const brickButton = document.querySelector("#brick");
+const clothButton = document.querySelector("#cloth");
+const katanaButton = document.querySelector("#katana");
 
-function gameLoop() {
-    for (let i = 0; i < ROUND_LENGTH; i++) {
-        const humanChoice = getHumanChoice();
-        const compChoice = getComputerChoice();
-        console.log(`Computer chose ${compChoice}`);
-        console.log(`Human chose ${humanChoice}`);
+const humanScoreUi = document.querySelector("#human-score");
+const compScoreUi = document.querySelector("#comp-score");
+const resultsContainer = document.querySelector(".results-container");
+const statusContainer = document.querySelector("#status-container");
 
-        playRound(compChoice, humanChoice);
-    }
 
-    getResults();
-}
+brickButton.addEventListener("click", () => {
+    const compChoice = getComputerChoice();
+
+    displayGameStatus(compChoice, "brick");
+    playRound(compChoice, "brick");
+});
+
+clothButton.addEventListener("click", () => {
+    const compChoice = getComputerChoice();
+
+    displayGameStatus(compChoice, "cloth");
+    playRound(compChoice, "cloth");
+});
+
+katanaButton.addEventListener("click", () => {
+    const compChoice = getComputerChoice();
+
+    displayGameStatus(compChoice, "katana");
+    playRound(compChoice, "katana");
+});
+
+// gameLoop();
+
+// function gameLoop() {
+//     const humanChoice = getHumanChoice();
+//     const compChoice = getComputerChoice();
+//     console.log(`Computer chose ${compChoice}`);
+//     console.log(`Human chose ${humanChoice}`);
+
+//     playRound(compChoice, humanChoice);
+
+//     getResults();
+// }
 
 function getComputerChoice() {
     const randomNum = Math.round(Math.random() * 10);
@@ -38,46 +68,96 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("brick, cloth or katana?");
+function displayGameStatus(compChoice, humanChoice) {
+    let compChoiceUi = document.querySelector("#comp-choice");
+    let humanChoiceUi = document.querySelector("#human-choice");
+    if (compChoiceUi === null) {
+        compChoiceUi = document.createElement("div");
+        compChoiceUi.setAttribute("id", "comp-choice");
 
-    if (choice === null ) {
-        throw new Error("Input something please.");
+        humanChoiceUi = document.createElement("div");
+        humanChoiceUi.setAttribute("id", "human-choice");
+
+        statusContainer.appendChild(compChoiceUi);
+        statusContainer.appendChild(humanChoiceUi);
     }
 
-    choice = choice.trim();
-    choice = choice.toLowerCase();
-    if (options.find((option) => option === choice) === undefined) {
-        throw new Error("Not a valid move bestie :))");
-    }
-
-    return choice;
+    compChoiceUi.textContent = `Computer chose ${compChoice}`;
+    humanChoiceUi.textContent = `Human chose ${humanChoice}`;
 }
 
 function playRound(computerChoice, humanChoice) {
+    roundCount++;
     if (computerChoice === humanChoice) {
-        console.log("It's a tie ¯\\_(ツ)_/¯");
-        console.log("----------------------------------------------------------------");
+        displayTie();
     } else if (computerChoice === options[BRICK]) {
-        humanWon(options[CLOTH], humanChoice, computerChoice) ? humanScore++ : computerScore++;
+        if (humanWon(options[CLOTH], humanChoice, computerChoice)) {
+            humanScore++;
+            humanScoreUi.textContent = `Human: ${humanScore}`;
+        } else {
+            computerScore++;
+            compScoreUi.textContent = `Computer: ${computerScore}`;
+        }
     } else if (computerChoice === options[CLOTH]) {
-        humanWon(options[KATANA], humanChoice, computerChoice) ? humanScore++ : computerScore++;
+        if (humanWon(options[KATANA], humanChoice, computerChoice)) {
+            humanScore++;
+            humanScoreUi.textContent = `Human: ${humanScore}`;
+        } else {
+            computerScore++;
+            compScoreUi.textContent = `Computer: ${computerScore}`;
+        }
     } else {
-        humanWon(options[BRICK], humanChoice, computerChoice) ? humanScore++ : computerScore++;
+        if (humanWon(options[BRICK], humanChoice, computerChoice)) {
+            humanScore++;
+            humanScoreUi.textContent = `Human: ${humanScore}`;
+        } else {
+            computerScore++;
+            compScoreUi.textContent = `Computer: ${computerScore}`;
+        }
     }
 }
 
 function humanWon(requiredWin, humanChoice, computerChoice) {
     if (requiredWin === humanChoice) {
-        console.log(`${humanChoice} beats ${computerChoice}!\nThe human won this round!!`);
-        console.log("----------------------------------------------------------------");
+        displayHumanWin(humanChoice, computerChoice)
         return true;
     } else {
-        computerScore++;
-        console.log(`${computerChoice} beats ${humanChoice}!\nThe computer won this round(come on you piece of flesh...)!!`);
-        console.log("----------------------------------------------------------------");
+        displayComputerWin(humanChoice, computerChoice);
         return false;
     }
+}
+
+function displayHumanWin(humanChoice, computerChoice) {
+    const statusContainer = document.querySelector("#status-container");
+    let statusMessage = document.querySelector("#status-message");
+    if (statusMessage === null) {
+        statusMessage = document.createElement("div");
+        statusMessage.setAttribute("id", "status-message");
+        statusContainer.appendChild(statusMessage);
+    }
+    statusMessage.textContent = `${humanChoice} beats ${computerChoice}!\nThe human won this round!!`;
+}
+
+function displayComputerWin(humanChoice, computerChoice) {
+    const statusContainer = document.querySelector("#status-container");
+    let statusMessage = document.querySelector("#status-message");
+    if (statusMessage === null) {
+        statusMessage = document.createElement("div");
+        statusMessage.setAttribute("id", "status-message");
+        statusContainer.appendChild(statusMessage);
+    }
+    statusMessage.textContent = `${computerChoice} beats ${humanChoice}!\nThe computer won this round(come on you piece of flesh...)!!`;
+}
+
+function displayTie() {
+    const statusContainer = document.querySelector("#status-container");
+    let statusMessage = document.querySelector("#status-message");
+    if (statusMessage === null) {
+        statusMessage = document.createElement("div");
+        statusMessage.setAttribute("id", "status-message");
+        statusContainer.appendChild(statusMessage);
+    }
+    statusMessage.textContent = "It's a tie ¯\\_(ツ)_/¯";
 }
 
 function getResults() {
